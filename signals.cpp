de6b38fd19fd3b548793907eb885f8b0248197a9
Kasie Madden <tuk33450@CIS-Linux2.temple.edu>
2021-04-14 17:31:30 -0400

    adding header file to clean up

--- signals.cpp
+++ signals.cpp
@@ -1,5 +1,46 @@
-#include <iostream>
+#include "signals.h"
+
+//Creation of the main process, the 3 signal generator processes, the 4 signal catcher processes 
+//and the reporter process. (one source for signal generator, one source for signal catcher).
+
+//Design and implementation of interval clocks
+
+
+//Create shared memory and attach to all processes
+
+
+
+struct shMemory{
+    int test;
+};
 
 int main() {
+
+int shmID = shmget(IPC_PRIVATE, sizeof(shMemory), IPC_CREAT | 0666);
+    if (shmID < 0){
+        cout<< "failed to create shared memory region"<<endl;
+    }
+
+    struct shMemory *shMemory;
+    shMemory = (struct shMemory *) shmat(shmID, NULL, 0); //get the shared memory
+
+    
+
+
+
+}//end of main()
+
+
+
+//signal generator
+void signalGen(){
+
+}
+
+void signalHan(){
+
+}
+
+void reporter(){
 
 }
---
+++ signals.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <signal.h>
+
+
+
+using namespace std;
